99f31764ff05285a076ac21a593004aa1674b061
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2011-09-24 00:21:47 +0200

    Better project names in homepage

--- plustoys/views.py
+++ plustoys/views.py
@@ -1,5 +1,4 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -8,12 +7,12 @@ def home(request):
     return render_to_response("plustoys/home.html", {
         "toys_list": 
         [{
-            "name": "User Badge Generator",
+            "name": "User Badge Generator for Google+",
             "url": reverse("plusbadges_home")
         },
         {
-            "name": "Trello Holodeck (only the name is fancy, the toy is very simplistic!)",
-            "url": "/hangoutapps/"
+            "name": "Trello Holodeck for Google+ Hangouts (only the name is fancy, the toy is very simplistic!)",
+            "url": "/hangoutapps/#h-trello-holodeck"
         }]
     }, context_instance=RequestContext(request))
 
